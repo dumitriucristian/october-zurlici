8ad8b011b63477a7e860a2e9be404d7a2037600d
cdumitriu <cdumitriu@pentalog.com>
2021-05-07 09:24:08 +0300

    refactor teacherForm

--- html/plugins/cdumitriu/teacher/components/TeacherForm.php
+++ html/plugins/cdumitriu/teacher/components/TeacherForm.php
@@ -3,15 +3,24 @@
     use cdumitriu\Teacher\Models\Teacher;
 
     use Cms\Classes\ComponentBase;
+    use Illuminate\Support\Facades\Redirect;
     use October\Rain\Exception\ValidationException;
     use October\Rain\Support\Facades\Validator;
     use October\Rain\Support\Facades\Input;
-    use October\Rain\Support\Facades\Flash;
     use Intervention\Image\Facades\Image;
     use Illuminate\Support\Facades\Request;
 
     class TeacherForm extends ComponentBase
     {
+        private $userName;
+        private $userSurname;
+        private $userEmail;
+        private $teacherName;
+        private $teacherSurname;
+        private $teacherDetails;
+        private $teacherSchool;
+        private $teacherCity;
+        private $teacherCounty;
 
         public function componentDetails()
         {
@@ -21,66 +30,30 @@
           ];
         }
 
-        public function onCreate()
+        private function validateInput()
         {
-
-
-
-            $userName = Input::get('user-name');
-            $userEmail = Input::get('user-email');
-            $teacherName = Input::get('teacher-name');
-            $teacherDetails = Input::get('teacher-details');
-            $teacherSchool = Input::get('teacher-school');
-            $teacherCity = Input::get('teacher-city');
-
-            $form = Input::all();
-
-            $rules = [
-                'user-name' => 'required',
-                'user-email' => 'required',
-                'teacher-name' => 'required',
-                'teacher-city' => 'required',
-                'teacher-school' => 'required',
-            ];
-
-            $image = $this->generateImage($teacherDetails);
-            $file = media_path('/teachers/base-image.jpg');
-            //validate user input
-            $validator = Validator::make($form,$rules);
-            if($validator->fails()) {
-                throw new ValidationException($validator);
-            }
-
-            return [
-                '#imageResult' => '<img src="'.$file. '">'
-            ];
-
-
-        }
-
-        public function onSend(){
-
-            $userName = Input::get('user-name');
-            $userSurname= Input::get('user-surname');
-            $userEmail = Input::get('user-email');
-            $teacherName = Input::get('teacher-name');
-            $teacherSurname = Input::get('teacher-surname');
-            $teacherDetails = Input::get('teacher-details');
-            $teacherSchool = Input::get('teacher-school');
-            $teacherCity = Input::get('teacher-city');
-            $teacherCounty = Input::get('teacher-county');
+            $this->userName = Input::get('user-name');
+            $this->userSurname= Input::get('user-surname');
+            $this->userEmail = Input::get('user-email');
+            $this->teacherName = Input::get('teacher-name');
+            $this->teacherSurname = Input::get('teacher-surname');
+            $this->teacherDetails = Input::get('teacher-details');
+            $this->teacherSchool = Input::get('teacher-school');
+            $this->teacherCity = Input::get('teacher-city');
+            $this->teacherCounty = Input::get('teacher-county');
 
             $form = Input::all();
 
             $rules = [
                 'user-name' => 'required',
-                'user-email' => 'required',
                 'user-surname' => 'required',
+                'user-email' => 'required',
                 'teacher-name' => 'required',
                 'teacher-surname' => 'required',
                 'teacher-city' => 'required',
                 'teacher-county' => 'required',
                 'teacher-school' => 'required',
+                'teacher-details' => 'required'
             ];
 
             $validator = Validator::make($form,$rules);
@@ -88,45 +61,63 @@
                 throw new ValidationException($validator);
             }
 
+        }
+
+        public function onCreate()
+        {
+            $this->validateInput();
+            $this->generateImage();
+            $image =  media_path('/teachers/base-image.jpg');
+            $this->page['preview_content'] = $image.'?id='.random_int(2,50);
+
+            return [
+                '#imageResult' => $this->renderPartial('preview')
+            ];
+        }
+
+
+
+        public function onSend(){
+
+            $this->validateInput();
             //@todo time function should be replaced with laravel time functions
-            //@todo refactor image save and name
+
             //saveImage
-            $basePath = 'app/media/teachers/';
-            $customName = $teacherName .'_'. $teacherSurname.'_'.time().'.jpg';
-            $imagePath = $basePath.$customName;
-            //var_dump($imagePath); die();
-            $this->generateImage($teacherDetails, $imagePath);
+            $customName = $this->teacherName .'_'. $this->teacherSurname.'_'.time().'.jpg';
+            $imagePath = strtolower( 'app/media/teachers/'.$customName);
+
+            $this->generateImage($imagePath);
 
             //save user data
             $teacher = new Teacher();
             $teacher->image = $customName;
-            $teacher->user_name = $userName;
-            $teacher->user_surname = $userSurname;
-            $teacher->user_email = $userEmail;
-            $teacher->teacher_name = $teacherName;
-            $teacher->teacher_surname = $teacherSurname;
-            $teacher->teacher_details = $teacherDetails;
-            $teacher->city = $teacherCity;
-            $teacher->teacher_county = $teacherCounty;
-            $teacher->school = $teacherSchool;
+            $teacher->user_name = $this->userName;
+            $teacher->user_surname = $this->userSurname;
+            $teacher->user_email = $this->userEmail;
+            $teacher->teacher_name = $this->teacherName;
+            $teacher->teacher_surname = $this->teacherSurname;
+            $teacher->teacher_details = $this->teacherDetails;
+            $teacher->city = $this->teacherCity;
+            $teacher->teacher_county = $this->teacherCounty;
+            $teacher->school = $this->teacherSchool;
             $teacher->year = 2021;
             $teacher->ip = Request::ip();
             // $teacher->letter = $letter;
             $teacher->save();
 
             // redirect user to teacher page;
-           return Flash::success("Am primit invatatoarea");
+            return Redirect::to('/succes?f='.$customName);
         }
 
 
-        private function generateImage($teacherDetails, $imagePath=null)
+        private function generateImage( $imagePath=null)
         {
             $originalImage = public_path('public/gift.jpeg');
 
             // create Image from file
             $img = Image::make($originalImage)->fit(600,600);
 
-            $lines = explode("\n", wordwrap($teacherDetails, 40));
+            $lines = explode("\n", wordwrap($this->teacherDetails, 40));
 
             for ($i = 0; $i < count($lines); $i++) {
                 $offset = 22 + ($i * 50);
@@ -138,7 +129,6 @@
                     $font->valign('top');
                 });
             }
-
 
             $fileName = (is_null($imagePath)) ? 'app/media/teachers/base-image.jpg' : $imagePath;
             $img->save(storage_path($fileName));
